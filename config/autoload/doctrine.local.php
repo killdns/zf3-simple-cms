859b931700a23cb86dc71bb8d8b95f94769b6943
Kirill <killdns@gmail.com>
2017-06-17 12:57:12 +0400

    remove gitignore in /config/autoload/

---
+++ config/autoload/doctrine.local.php
@@ -0,0 +1,19 @@
+<?php
+return array(
+    'doctrine' => array(
+        'connection' => array(
+            // default connection name
+            'orm_default' => array(
+                'driverClass' => 'Doctrine\DBAL\Driver\PDOMySql\Driver',
+                'params' => array(
+                    'host'     => 'localhost',
+                    'port'     => '3306',
+                    'user'     => 'root',
+                    'password' => '',
+                    'dbname'   => 'DBNAME',
+                    'charset' => 'UTF8'
+                )
+            )
+        )
+    ),
+);
